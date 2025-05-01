cd8cca848d1f10678f1c9efcf554df50ee26ba33
James Chen <chenhao540@gmail.com>
2025-05-01 10:31:57 -0700

    init

---
+++ init.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.keymap.set("i", "<C-s>", "<cmd>w<cr><Esc>", { desc = "insert mode, save file" })
+vim.keymap.set("n", "<C-s>", "<cmd>w<cr>", { desc = "normal mode, save file" })
+vim.keymap.set("n", "<leader>w", "a<space><esc>", { desc = "normal mode, insert a whitespace" })
